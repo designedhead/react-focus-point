5f463ab44fc76ad0d6c68b278e90b342249f77f4
Rafael Mendes <rafael.mendes@lush.co.uk>
2025-04-08 13:26:49 +0100

    fix: build issues

---
+++ rollup.config.mjs
@@ -0,0 +1,36 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+import external from 'rollup-plugin-peer-deps-external';
+import dts from 'rollup-plugin-dts';
+import { readFileSync } from 'fs';
+
+const packageJson = JSON.parse(readFileSync('./package.json', 'utf8'));
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+        name: 'reactFocusPoint'
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true
+      }
+    ],
+    plugins: [external(), resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), terser()],
+    external: ['react', 'react-dom', 'clsx', 'tailwind-merge']
+  },
+  {
+    input: 'dist/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+    external: [/\.css$/]
+  }
+];
